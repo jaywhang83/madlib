03644aac218d70910e8c0f61182a78252b939e2a
Gracie Lundell and Jay Whang <jaywhang83@gmail.com>
2016-01-13 13:16:37 -0800

    Refactor js

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,11 @@
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
+    blanks.forEach(function(index) {
+      var userInput = $("input#" + index).val();
+      $("." + index).text(userInput);
+    });
 
     $("#story").show();
 
