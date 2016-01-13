0c12e2ff13efc743e7aa9755a8490b06c647afdc
Gracie Lundell and Jay Whang <gracielundell@gmail.com>
2016-01-13 13:26:12 -0800

    Refactor using for loop

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,16 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
+    for (var index = 0; index < blanks.length; index++) {
+      var userInput = $("input#" + blanks[index]).val();
+      $("." + blanks[index]).text(userInput);
+    };
+    /*
     blanks.forEach(function(index) {
       var userInput = $("input#" + index).val();
       $("." + index).text(userInput);
     });
-
+    */
     $("#story").show();
 
     event.preventDefault();
